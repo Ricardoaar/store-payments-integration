3b827810261e218b9dea83793d81f79173c8fa71
caprun <26richardr@gmail.com>
2022-02-07 13:06:47 -0600

    pre-project: dependences and configurations
    dependences installed:
    - npm/tailwindcss
    configuration:
    - docker-compose file added
    - env variables for place2pay
    - tailwindcss files configured
    - cleaned welcome blade view

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/**/.blade.php",
+        "./resources/**/*.js",
+
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
